940be8ce2894a52897477efd1f3fced1d41b522f
Emil Asadov <Emil.Asadov@rabitabank.com>
2022-02-14 18:14:56 +0400

    Add new comment

--- ConsoleApp_GitHub_Main/Program.cs
+++ ConsoleApp_GitHub_Main/Program.cs
@@ -10,6 +10,13 @@ namespace ConsoleApp_GitHub_Main
     {
         static void Main(string[] args)
         {
+            var x = 2 * 15 + 100 / 2;
+            var res = "Tek";
+            if (x % 2 == 0)
+                res = "Cut";
+
+            Console.Write($"{"Netice: "}{x}{Environment.NewLine}{res}");
+            Console.ReadKey();
         }
     }
 }
